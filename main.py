c2d2621eeb4cda3911ec65048446548cf520e05b
taozi4887 <321joemama123@gmail.com>
2026-02-07 03:21:08 -0500

    v0.7.2: Fix missing time import and wave module in PyInstaller

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ except ImportError:
     AUTO_UPDATE_AVAILABLE = False
     print("Auto-update not available: requests library not installed")
 
-__version__ = "0.7.1"
+__version__ = "0.7.2"
 
 # Settings management
 class Settings:
