445812c325ae63b7ac5f844fd2e04425b6d47f35
taozi4887 <321joemama123@gmail.com>
2026-02-07 05:58:44 -0500

    v0.7.22: Remove initialize_levels_from_osz call, fix checkmark encoding

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ except ImportError:
     AUTO_UPDATE_AVAILABLE = False
     print("Auto-update not available: requests library not installed")
 
-__version__ = "0.7.21"
+__version__ = "0.7.22"
 
 # Settings management
 class Settings:
