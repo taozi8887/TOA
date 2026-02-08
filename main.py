2573cea5f5f939176e2367cc7a7b87f11f7f92cc
taozi4887 <321joemama123@gmail.com>
2026-02-08 05:15:22 -0500

    v0.8.1

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ except ImportError:
     AUTO_UPDATE_AVAILABLE = False
     print("Auto-update not available: requests library not installed")
 
-__version__ = "0.8.0"
+__version__ = "0.8.1"
 
 # Settings management
 class Settings:
